9d96ac97085934cf74af58d54be0c44878ae8005
Stheeven Quishpe <stheeven.quishpe@udla.edu.ec>
2023-10-16 23:15:32 -0500

    Se izo

--- Ejercicio21.c
+++ Ejercicio21.c
@@ -4,3 +4,24 @@ int main(void){
 
 int num;
 int suma=0;
+
+printf("Coloque un numero entero\n");
+scanf("%d", &num);
+
+if (num>0)
+{
+    while (num>0){
+
+suma+= num % 10;
+num /= 10;
+
+}
+
+printf("El resultado de la suma de sus numeros es %d\n", suma);
+}
+
+else {
+    printf("El numero colocado es un numero negativo\n");
+}
+
+}
