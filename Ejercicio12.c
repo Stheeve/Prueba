357d656bf0625da50482c04e5f3602841c7b146f
Stheeven Quishpe <stheeven.quishpe@udla.edu.ec>
2023-10-16 23:08:55 -0500

    Se incluyo un if para que solo se incluyan numeros de tres dijitos

--- Ejercicio12.c
+++ Ejercicio12.c
@@ -10,7 +10,8 @@ int aux=0;
 printf("Coloque un numero entero de 3 dijitos\n");
 scanf("%d", &num);
 
-while(num>0){
+if (num>=100 && num <=999){
+    while(num>0){
         digito=num % 10;
         if (digito==1){
             aux=1;
@@ -26,3 +27,5 @@ while(num>0){
 
         printf("El numero no tiene el digito 1\n");
     }
+
+}
